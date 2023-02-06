1bcaaf26a6d8bd6bfff3da62e5fcb2214a3c1769
Dalton <lambertd757@gmail.com>
2023-02-06 18:24:08 -0500

    Added functionality to search for weather information by location name

--- src/main/java/com/dalton/WeatherApp/controller/WeatherController.java
+++ src/main/java/com/dalton/WeatherApp/controller/WeatherController.java
@@ -39,7 +39,7 @@ public class WeatherController {
         String apiKey = System.getenv("API_KEY");
 
         // Define the cities that you want to retrieve weather information for
-        String[] cities = {"London", "Paris", "Florida", "Tokyo", "Tampa", "New York"};
+        String[] cities = {"London", "Paris", "Florida", "Tokyo", "Tampa", "New York", "Miami"};
 
         // Use the RestTemplate to make a request to the OpenWeatherMap API for each city
         RestTemplate restTemplate = new RestTemplate();
@@ -69,8 +69,25 @@ public class WeatherController {
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/location/{location}", method = RequestMethod.GET)
-    public List<Weather> findByLocation(@PathVariable String location){
-        return weatherDao.findByLocation(location);
+    public Weather findByLocation(@PathVariable String location){
+        // Define the API endpoint URL and your API key
+        String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+        String apiKey = System.getenv("API_KEY");
+
+        // Use the RestTemplate to make a request to the OpenWeatherMap API for the specified location
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> response = restTemplate.getForObject(apiUrl, Map.class, location, apiKey);
+
+        // Extract the location name and temperature from the response
+        String locationName = (String) response.get("name");
+        double temperature = (double) ((Map<String, Object>) response.get("main")).get("temp");
+
+        // Convert the temperature from Kelvin to Fahrenheit
+        double temperatureFahrenheit = (temperature - 273.15) * 9 / 5 + 32;
+
+        // Create a new Weather object and return it
+        return new Weather(locationName, temperatureFahrenheit);
     }
+
 
 }
